11026f07b6c4b1a54163f0cf554c47a954c77f90
Hüseyin Öztürk <139119853+ozturkhuseyin@users.noreply.github.com>
2024-03-11 02:15:04 +0300

    Example Update & new CH32V003 example (#9)
    * created examples directory and moved examples
    
    * added CH32V003 example

---
+++ examples/CH32V003F4P6_PetitModbus/obj/User/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# MRS Version: {"version":"1.8.5","date":"2023/05/22"}
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../User/ch32v00x_it.c \
+../User/main.c \
+../User/system_ch32v00x.c 
+
+OBJS += \
+./User/ch32v00x_it.o \
+./User/main.o \
+./User/system_ch32v00x.o 
+
+C_DEPS += \
+./User/ch32v00x_it.d \
+./User/main.d \
+./User/system_ch32v00x.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+User/%.o: ../User/%.c
+	@	@	riscv-none-embed-gcc -march=rv32ecxw -mabi=ilp32e -msmall-data-limit=0 -msave-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused -Wuninitialized  -g -I"C:\MRS_DATA\workspace\CH32V003F4P6_PetitModbus\Debug" -I"C:\MRS_DATA\workspace\CH32V003F4P6_PetitModbus\Core" -I"C:\MRS_DATA\workspace\CH32V003F4P6_PetitModbus\User" -I"C:\MRS_DATA\workspace\CH32V003F4P6_PetitModbus\Peripheral\inc" -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@	@
+
